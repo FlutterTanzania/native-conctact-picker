3130cde0f863ddf7cf2b64368d49184f1f57b694
ProsperChihimba <prosperchihimba@gmail.com>
2023-02-02 00:01:03 +0400

    Initial commit with ios section

---
+++ android/app/src/main/kotlin/com/example/contact_specific/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.contact_specific
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _contact = 'Unknown';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(
+              color: Colors.red,
+              textColor: Colors.white,
+              child: const Text('Picker'),
+              onPressed: () => _getAContact(),
+            ),
+            Text(_contact ?? '')
+          ],
+        ),
+      ),
+    );
+  }
+
+  _getAContact() async {
+    String contact;
+    try {
+      contact = await FlutterIsAwesome.getAContact();
+    } on PlatformException {
+      contact = 'Failed to get contact.';
+    }
+    if (!mounted) return;
+    setState(() {
+      _contact = contact;
+    });
+  }
+}
+
+class FlutterIsAwesome {
+  static const MethodChannel _channel = MethodChannel('com.prosper.specific');
+  static Future<String> getAContact() async {
+    final String contact = await _channel.invokeMethod('getAContact');
+    return contact;
+  }
+}
